171f3ef17252c395d727296f387bdb8067a9b0dd
TheHyper-Dev <suleymanyigitdarcan@gmail.com>
2025-09-15 11:53:54 +0200

    Fix: Created a .env.local to prevent hardcodings

--- obj/Debug/net9.0/MinimalApiBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/MinimalApiBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalApiBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e39368a49ae85750c527559e823d5c70d89535d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c7cb73b35b362fdccefc52c2426412be0f27a2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalApiBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalApiBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
